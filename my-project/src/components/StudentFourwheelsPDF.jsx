6b11e47a98042d2c3651f99bd6e145e87bdaf691
Jesner Perillo <perillojesner15@gmail.com>
2024-08-23 15:19:51 +0800

    Added Student Fourwheeler PDF generator

---
+++ my-project/src/components/StudentFourwheelsPDF.jsx
@@ -0,0 +1,86 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import jsPDF from 'jspdf';
+import 'jspdf-autotable';
+
+export default function StudentFourwheelsPDF() {
+  const [students, setStudents] = useState([]);
+  const [error, setError] = useState('');
+  const [vehicleCounts, setVehicleCounts] = useState([]);
+
+  useEffect(() => {
+    const fetchStudentData = async () => {
+      try {
+        const response = await axios.get('http://localhost/website/my-project/Backend/fetchstudentsdata.php', {
+          withCredentials: true,
+        });
+
+        console.log('Student data response:' + response.data);
+
+        if (response.data.success) {
+          const fourwheelerStudents = response.data.students.filter(student => student.Vehicle === 'Fourwheeler');
+          setStudents(fourwheelerStudents);
+
+          if (response.data.vehicleCounts) {
+            const vehicleCounts = {};
+            for (const [vehicle, count] of Object.entries(response.data.vehicleCounts)) {
+              vehicleCounts[vehicle] = Number(count);
+            }
+            setVehicleCounts(vehicleCounts);
+          } else {
+            setVehicleCounts({});
+          } 
+        } else {
+          setError(response.data.message || 'No data found for the logged-in user.');
+        }
+      } catch (error) {
+        setError('Error fetching data:'+ error.message);
+        console.error('Error fetching student data:', error);
+      }
+    };
+
+    fetchStudentData();
+  }, []);
+
+  const generatePDF = () => {
+    const doc = new jsPDF();
+
+    doc.text('FourWheeler Students Data', 10, 10);
+
+    const tableColumn = ["#", "Student Number", "Name", "Email", "Vehicle", "Plate Number", "Parking Slot"];
+    const tableRows = [];
+
+    students.forEach((student, index) => {
+      const studentData = [
+        index + 1,
+        student['Student Number'],
+        student.Name,
+        student.Email,
+        student.Vehicle,
+        student['Plate Number'],
+        student.slot_number,
+      ];
+      tableRows.push(studentData);
+    });
+
+    doc.autoTable({
+      head: [tableColumn],
+      body: tableRows,
+      theme: 'grid',
+      headStyles: { fillColor: [0, 0, 102] },
+      styles: { fontSize: 10 },
+    });
+
+    doc.save('Student-FourWheeler.pdf');
+  };
+
+
+  return(
+    <>  
+      <div className="relative w-full h-full flex flex-col items-center">
+        <span className="text-4xl mt-10">{vehicleCounts['Fourwheeler'] || 0}/20</span>
+        <button className="w-full h-1/4 bg-red-600 rounded text-white absolute bottom-0 mb-1" onClick={generatePDF}>Generate PDF</button>
+      </div>
+    </>
+  );
+}
