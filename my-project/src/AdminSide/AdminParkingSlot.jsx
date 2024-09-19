b0ff3341124d1a28294248ca8be1a0b2dd178ce7
Jesner Perillo <perillojesner15@gmail.com>
2024-09-19 22:22:21 +0800

    Modified the popup images

--- my-project/src/AdminSide/AdminParkingSlot.jsx
+++ my-project/src/AdminSide/AdminParkingSlot.jsx
@@ -1097,7 +1097,7 @@ useEffect(() => {
       
             {isModalOpen && (
               <div className="fixed w-full h-full inset-0 z-20 flex items-center justify-center bg-black bg-opacity-50">
-                <div className="relative bg-white p-4 rounded-lg shadow-lg flex justify-center w-2/4 h-2/3">
+                <div className="relative bg-white p-1 rounded-lg shadow-lg flex justify-center w-full h-auto">
                   <button
                     onClick={handleCloseModal}
                     className="absolute top-0 right-0 mt-2 mr-2 text-gray-500 hover:text-gray-700"
