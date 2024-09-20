f35e0a763570415462362c384aff48cb034d5ef0
Jesner Perillo <perillojesner15@gmail.com>
2024-09-20 22:53:24 +0800

    Add show password to students form

--- my-project/src/StudentSide/StudentLogin.jsx
+++ my-project/src/StudentSide/StudentLogin.jsx
@@ -5,6 +5,7 @@ import URSlogo from '../Pictures/urs.png';
 import GSOlogo from '../Pictures/gsoo.png';
 import '../App.css';
 import SideImg from '../Pictures/sideimg.png';
+import { IoEyeOff, IoEye  } from "react-icons/io5";
 
 export default function StudentLogin() {
     const [studentNumber, setStudentNumber] = useState('');
@@ -15,6 +16,11 @@ export default function StudentLogin() {
     const [email, setEmail] = useState('');
     const [message, setMessage] = useState('');
     const [isVisible, setIsVisible] = useState(false);
+    const [showPassword, setShowPassword] = useState(false);
+
+    const togglePassword = () => {
+        setShowPassword(!showPassword);
+    }
 
     const handleOpenModal = () => {
         setIsVisible(true);
@@ -123,13 +129,18 @@ export default function StudentLogin() {
                             value={password}
                             onChange={(e) => setPassword(e.target.value)}
                             className="bg-gray-800 text-white w-full py-3 px-3.5 outline-none border border-gray-600 rounded-md peer sm:py-2 sm:px-2.5"
-                            type="password"
+                            type={showPassword ? 'text' : 'password'}
                             placeholder=" "
                             required
                         />
                         <span className="text-gray-500 absolute left-3.5 top-3 transform -translate-y-1/2 transition-all duration-300 ease peer-placeholder-shown:top-1/2 peer-placeholder-shown:text-sm peer-focus:top-3 peer-focus:text-xs peer-focus:text-cyan-500 sm:left-2.5 sm:text-xs">
                             Password
                         </span>
+                        <button
+                            onClick={togglePassword}
+                            className="absolute right-5 top-5">
+                            {showPassword ? <IoEyeOff className="w-6 h-6"/> : <IoEye className="w-6 h-6"/>}
+                        </button>
                     </label>
                     <button
                         type="submit"
