da4737098f97c903696414fe4936949fe121610c
Jesner Perillo <perillojesner15@gmail.com>
2024-09-26 21:01:28 +0800

    Modified the faculty signup UI

--- my-project/src/FacultyStaffSide/FacultyStaffSignup.jsx
+++ my-project/src/FacultyStaffSide/FacultyStaffSignup.jsx
@@ -73,11 +73,23 @@ export default function FacultyStaffSignup(){
   
   return(
     <>
-      <div className="bg-blue-700 min-h-screen flex flex-col items-center">
-      <div className="relevant form xl:w-2/3 mt-3 justify-between rounded-xl h-4/5 sm:flex max-sm:flex-column max-sm:text-center max-sm:w-full ">
-        <div className="header relative flex flex-col items-center justify-center space-y-24 w-2/4 h-auto py-4 max-sm:w-full">
-            <img src={SideImg} alt="URS Logo" className="w-98 h-98 z-20" />
+      <div className="bg-gradient-to-r from-blue-700 w-full to-teal-700 min-h-screen flex flex-col items-center">
+      <div className="form xl:w-2/3 mt-16 justify-between rounded-xl h-4/5 sm:flex max-sm:flex-column max-sm:text-center max-sm:w-full bg-blue-900">
+      <div className="header flex flex-col items-center justify-center space-y-24 w-2/4 h-auto py-4 max-sm:w-full">
+        <div className="gso-logo flex items-center justify-center w-full md:w-1/3">
+          <img src={GSOlogo} alt="GSOLogo" className="w-24 h-24 md:w-36 md:h-36" />
         </div>
+        <div className="gso-title flex items-center justify-center w-full md:w-1/3 text-center md:text-left">
+          <h1 className="text-white font-bold text-2xl md:text-3xl lg:text-5xl">
+            <span className="text-4xl md:text-6xl tracking-wider">G</span>eneral{' '}
+            <span className="text-4xl md:text-6xl tracking-wider">S</span>ervices{' '}
+            <span className="text-4xl md:text-6xl tracking-wider">O</span>ffice
+          </h1>
+        </div>
+        <div className="urs-logo flex items-center justify-center w-full md:w-1/3">
+          <img src={URSlogo} alt="URS Logo" className="w-20 h-24 md:w-28 md:h-32" />
+        </div>
+      </div>
         <form onSubmit={handleSubmit} className="flex flex-col w-2/4 gap-3 float:right h-auto max-w-3xl p-6 rounded-2xl relative bg-gray-900 text-white border border-gray-700 max-sm:w-full sm:p-5">
             <p className="text-3xl font-semibold tracking-tight relative flex items-center pl-7 text-cyan-500 sm:text-2xl">
             Register (Faculty and Staff)
