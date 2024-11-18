680162de5683de68b440e9e15a808729e540a7b5
Jesner Perillo <perillojesner15@gmail.com>
2024-11-18 20:31:11 +0800

    Removed the text for ORCR

--- my-project/src/FacultyStaffSide/FacultyStaffSignup.jsx
+++ my-project/src/FacultyStaffSide/FacultyStaffSignup.jsx
@@ -156,7 +156,7 @@ export default function FacultyStaffSignup(){
               <input name="license" class="form-control block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm" type="file" id="formFileLicense" onChange={handleFileChange}/>
             </div>
             <div class="w-full lg:w-1/2">
-              <label for="formFileOrcr" class="form-label block text-sm font-medium text-gray-700">ORCR<span className="text-[7px]">(Original Receipt/Certificate of Registration)</span></label>
+              <label for="formFileOrcr" class="form-label block text-sm font-medium text-gray-700">ORCR</label>
               <input name="orcr" class="form-control block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm" type="file" id="formFileOrcr" onChange={handleFileChange}/>
             </div>
           </div>
